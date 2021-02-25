4ad36456b58f514a18a528588125c70c75a4a068
Lucas Moreira da Cruz <moreira552@msn.com>
2021-02-25 15:28:15 +0000

    Add project files.

---
+++ KafkaConsumer.Demo.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Confluent.Kafka;
+using static System.Console;
+
+namespace KafkaConsumer.Demo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Escreva o tópico que deseja ver as mensagens:");
+            var topic = ReadLine();
+
+            var config = new ConsumerConfig
+            {
+                GroupId = "gid-consumers",
+                BootstrapServers = "localhost:9092"
+            };
+
+            using (var consumer = new ConsumerBuilder<Null,string>(config).Build())
+            {
+                consumer.Subscribe(topic);
+                while(true)
+                {
+                    var cr = consumer.Consume();
+                    WriteLine(cr.Message.Value);
+                }
+            }
+        }
+    }
+}
